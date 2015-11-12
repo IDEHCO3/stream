0f63e520688dfee0d5c8c67c90e3ed4182d93157
RogerioBorba <idehco3@gmail.com>
2015-11-11 23:11:28 -0200

    several bugs

--- stream/settings.py
+++ stream/settings.py
@@ -122,3 +122,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'auth.User'
+
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
--- twitter_stream/tasks.py
+++ twitter_stream/tasks.py
@@ -1,3 +1,14 @@
+from celery.decorators import task
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+@task(name="send_feedback_email_task")
+def send_feedback_email_task(email, message):
+    """sends an email when feedback form is filled successfully"""
+    logger.info("Sent feedback email")
+    return send_feedback_email(email, message)
+
 from celery import task
 from twython import Twython
 from twitter_stream.models import MonitorTwitter, GeoTwitter
